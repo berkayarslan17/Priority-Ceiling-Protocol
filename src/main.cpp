96f7fc5135b94367d2dd7202fed1885e9c71029e
Berkay Arslan <berkay.arslan@bordatech.com>
2022-05-23 12:59:36 +0300

    Test the system's avoid deadlock functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,50 @@
 #include "mutex.h"
 #include <iostream>
-#include <pthread.h>
 
-void *func(void *arg)
+gtu::mutex m1(10000), m2(5000);
+
+void *threadWork1(void *ptr)
 {
-    // detach the current thread
-    // from the calling thread
-    pthread_detach(pthread_self());
+    for (;;)
+    {
+        std::lock_guard<gtu::mutex> lock1(m1);
+        printf("Mutex m1 has been taken in thread1\n");
+        std::lock_guard<gtu::mutex> lock2(m2);
+        printf("Mutex m2 has been taken in thread1\n");
+    }
+}
 
-    printf("Inside the thread\n");
-
-    // exit the current thread
-    pthread_exit(NULL);
+void *threadWork2(void *ptr)
+{
+    for (;;)
+    {
+        std::lock_guard<gtu::mutex> lock2(m2);
+        printf("Mutex m2 has been taken in thread2\n");
+        std::lock_guard<gtu::mutex> lock1(m1);
+        printf("Mutex m1 has been taken in thread2\n");
+    }
 }
 
 int main()
 {
-    pthread_t ptid;
+    pthread_t thread1, thread2;
 
-    // Creating a new thread
-    pthread_create(&ptid, NULL, &func, NULL);
-    printf("This line may be printed"
-           " before thread terminates\n");
+    /* Create independent threads each of which will execute function */
+
+    // m1.registerThread(thread1);
+    // m1.registerThread(thread2);
+    // m2.registerThread(thread1);
+    // m2.registerThread(thread2);
+
+    pthread_create(&thread1, NULL, threadWork1, NULL);
+    pthread_create(&thread2, NULL, threadWork2, NULL);
+
+    /* Wait till threads are complete before main continues. Unless we  */
+    /* wait we run the risk of executing an exit which will terminate   */
+    /* the process and all threads before the threads have completed.   */
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return 0;
 }
