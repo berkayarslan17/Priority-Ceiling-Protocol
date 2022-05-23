6d7c8b927345099b4c49162af1d5b2e1f20c907e
Berkay Arslan <berkay.arslan@bordatech.com>
2022-05-23 13:00:37 +0300

    Implement gtu::mutex class with Priority Ceiling Protocol

--- src/mutex.cpp
+++ src/mutex.cpp
@@ -1,1 +1,65 @@
 #include "mutex.h"
+
+thread_local unsigned long gtu::mutex::this_thread_ceiling_value(ULONG_MAX);
+
+void gtu::mutex::lock()
+{
+    if (check_for_ceiling_violation())
+    {
+        internal_mutex.lock();
+    }
+
+    update_ceiling_value();
+}
+
+void gtu::mutex::unlock()
+{
+    this_thread_ceiling_value = prev_ceiling_value;
+    internal_mutex.unlock();
+}
+
+bool gtu::mutex::try_lock()
+{
+    if (check_for_ceiling_violation())
+    {
+        if (!internal_mutex.try_lock())
+        {
+            return false;
+        }
+
+        update_ceiling_value();
+        return true;
+    }
+
+    return false;
+}
+
+bool gtu::mutex::check_for_ceiling_violation()
+{
+    if (this_thread_ceiling_value <= ceiling_value)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+void gtu::mutex::update_ceiling_value()
+{
+    prev_ceiling_value = this_thread_ceiling_value;
+    this_thread_ceiling_value = ceiling_value;
+}
+
+// void gtu::mutex::register_thread(pthread_t thread)
+// {
+//     sched_param sch;
+//     int policy, threadPriority;
+//     pthread_getschedparam(thread.native_handle(), &policy, &sch);
+//     // Get thread priority for assigning the ceiling value
+//     threadPriority = sch.sched_priority;
+
+//     if (ceiling < threadPriority)
+//     {
+//         ceiling = threadPriority;
+//     }
+// }
--- src/mutex.h
+++ src/mutex.h
@@ -2,15 +2,34 @@
 #define _MUTEX_H
 
 #include <mutex>
+#include <climits>
+#include <pthread.h>
 
 namespace gtu
 {
     class mutex
     {
     private:
-        std::mutex mutex;
+        std::mutex internal_mutex;
+        unsigned int const ceiling_value;
+        unsigned int prev_ceiling_value;
+        static thread_local unsigned long this_thread_ceiling_value;
+
+        bool check_for_ceiling_violation();
+        void update_ceiling_value();
+
+    public:
+        explicit mutex(unsigned long value) : ceiling_value(value),
+                                              prev_ceiling_value(0)
+        {
+        }
+
+        void register_thread(pthread_t thread);
+        void lock();
+        void unlock();
+        bool try_lock();
     };
 
-};
+}
 
 #endif
